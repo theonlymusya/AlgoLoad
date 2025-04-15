077406efa189ab129df1248126a705f3de29fe03
Gleb Skryabin <skryabin.gleb.dev@gmail.com>
2025-04-15 16:02:08 +0300

    refactor(gunicorn): move gunicorn configuration to a separate file

--- boot2.sh
+++ boot2.sh
@@ -10,8 +10,5 @@ readonly LOG_FILE="$LOG_FOLDER/microbial.log"
 mkdir -p $LOG_FOLDER
 touch $LOG_FILE
 
-# launch web server
-exec gunicorn -b 0.0.0.0:3001 --timeout 300 --access-logfile $LOG_FILE --error-logfile $LOG_FILE flask_skipod:appFlask
-
-# launch web server without logs (tmp)
-# exec gunicorn -b 0.0.0.0:3001 --timeout 300 flask_skipod:appFlask
+# launch web server with config file
+exec gunicorn --config gunicorn_config.py flask_skipod:appFlask
--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,10 +1,20 @@
-# import multiprocessing
+import multiprocessing
 
-# workers = multiprocessing.cpu_count() * 2 + 1
-bind = 'unix:flaskrest.sock'
+workers = multiprocessing.cpu_count() * 2 + 1
+threads = 2
+worker_class = "gthread"
+timeout = 300
+keepalive = 120
+max_requests = 100
+max_requests_jitter = 10
+
+bind = "0.0.0.0:3001"
 umask = 0o007
 reload = True
 
-#logging
-accesslog = '-'
-errorlog = '-'
+# logging
+accesslog = "logs/microbial.log"
+errorlog = "logs/microbial.log"
+loglevel = "info"
+capture_output = True
+enable_stdio_inheritance = True
